9b7d5faefa429f7ae76d63495ec83c81ca5314b7
KrittidejDev <krittidej.dev@gmail.com>
2025-05-05 18:11:07 +0700

    edit login btn

--- src/components/Forms/loginForm.tsx
+++ src/components/Forms/loginForm.tsx
@@ -70,9 +70,9 @@ export function LoginForm({ className, onSubmit, ...props }: LoginFormProps) {
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <Button variant="outline" className="w-full">
+              {/* <Button variant="outline" className="w-full">
                 Login with Google
-              </Button>
+              </Button> */}
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
